13d8b2f17387e98e65f8543d2d365790824b0cf6
ecoronel3 <ezequiel.f.coronel@gmail.com>
2016-09-17 19:45:56 -0300

    Funcion de calc distancia

--- wifindApp/calculador_distancias.js
+++ wifindApp/calculador_distancias.js
@@ -3,7 +3,23 @@ module.exports =
      function() {
 
         this.calcular = function(ubicacion1, ubicacion2) {
-            return 400;
+            var dist = this.distance(ubicacion1.latitud, ubicacion1.longitud, ubicacion2.latitud, ubicacion2.longitud) ;
+            return dist;
         };
 
+        // Funcion de distancia aproximada. No tiene en cuenta si
+        // hay que doblar la calle o ese tipo de problemas de la vida real.
+        // Es mas bien de juguete pero en nuestro contexto, sirve.
+        this.distance = function (lat1, lon1, lat2, lon2) {
+        	var radlat1 = Math.PI * lat1/180;
+        	var radlat2 = Math.PI * lat2/180;
+        	var theta = lon1-lon2;
+        	var radtheta = Math.PI * theta/180;
+        	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        	dist = Math.acos(dist);
+        	dist = dist * 180/Math.PI;
+        	dist = dist * 60 * 1.1515;
+        	dist = dist * 1609.344; // a metros
+        	return dist
+        }
     };
