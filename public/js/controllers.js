0833692678327e8cdf93aded3b275c681ab7b1e4
nico <klascolinu@gmail.com>
2016-09-17 22:00:06 -0300

    Agrego una posible implementacion con geocoder para la conversion a coordenadas

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -27,6 +27,25 @@ angular.module('wifindAppControllers', [])
         console.log("Inicio");
     })
 
+    .controller('BuscarDir',function($scope,uiGmapGoogleMapApi){
+         uiGmapGoogleMapApi.then(function(maps) {
+         var map = $scope.map;
+         var geocoder = new google.maps.Geocoder();
+         var address = document.getElementById('address').value;
+         geocoder.geocode( { 'address': address}, function(results, status) {
+         if (status == 'OK') {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  
+
+
     .controller('BaresCtrl', function($scope, $http) {
 
         $scope.barData = {}
