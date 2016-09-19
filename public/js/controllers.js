33fb3b40746fa85c3660c0ac71cf7d0f56601db2
nico <klascolinu@gmail.com>
2016-09-18 22:15:53 -0300

    Agrego funcionalidad al loginctrl

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -127,16 +127,47 @@ angular.module('wifindAppControllers', [])
         });
     })
 
-    .controller('LoginCtrl', function($scope) {
-        console.log('hola');
+    .controller('LoginCtrl', function($scope,$http) {
+
+        console.log('Login');
         $scope.showLogin = true;
 
         $scope.login = function() {
             $scope.showLogin = true;
+            console.log("logueando...")
+             $http({
+                method: 'POST',
+                url: 'http://localhost:5000/api/user/:email',
+                data: {
+                    email: $scope.regEmail
+                }
+
+            }).then(function successCallback(response) {
+                console.log("listo")
+
+            }, function errorCallback(response) {
+                console.log("error!")
+            });
         }
 
         $scope.register = function() {
             $scope.showLogin = false;
+            console.log("Registrando...");
+                $http({
+                method: 'POST',
+                url: 'http://localhost:5000/api/user',
+                data: {
+                    email: $scope.userEmail,
+                    nombre:$scope.userName,
+                    password:$scope.userPass
+                }
+            }).then(function successCallback(response) {
+                console.log("listo")
+
+            }, function errorCallback(response) {
+                console.log("error!")
+            });
+
         }
     })
 
