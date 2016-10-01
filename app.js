8c27302785419baca3d976092bbc2e8c83c20289
famartinez90 <famfede@gmail.com>
2016-10-01 18:47:11 -0300

    Limpia el app.js

--- app.js
+++ app.js
@@ -41,8 +41,6 @@ angular
     .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, site, $log, $window, $http, $mdToast, $state) {
         $state.go('Inicio');
         $scope.url = site.protocol + "://" + site.domain + ":" + site.port;
-        $scope.toggleLeft = buildDelayedToggler('left');
-        $scope.toggleRight = buildToggler('right');
         $scope.googleSignedIn = false;
 
         $scope.isRegistered = false;
@@ -89,64 +87,11 @@ angular
                 });
         }
 
-        $window.onSignIn = onSignIn;
-
-        /**
-        * Supplies a function that will continue to operate until the
-        * time is up.
-        */
-        function debounce(func, wait, context) {
-            var timer;
-            return function debounced() {
-                var context = $scope,
-                args = Array.prototype.slice.call(arguments);
-                $timeout.cancel(timer);
-                timer = $timeout(function() {
-                    timer = undefined;
-                    func.apply(context, args);
-                }, wait || 10);
-            };
-        }
-        /**
-        * Build handler to open/close a SideNav; when animation finishes
-        * report completion in console
-        */
-        function buildDelayedToggler(navID) {
-            return debounce(function() {
-                // Component lookup should always be available since we are not using `ng-if`
-                $mdSidenav(navID)
-                .toggle()
-                .then(function () {
-                    //$log.debug("toggle " + navID + " is done");
-                });
-            }, 200);
-        }
-        function buildToggler(navID) {
-            return function() {
-                // Component lookup should always be available since we are not using `ng-if`
-                $mdSidenav(navID)
-                .toggle()
-                .then(function () {
-                    //$log.debug("toggle " + navID + " is done");
-                });
-            }
-        }
-    })
-
-    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log,uiGmapGoogleMapApi, $state) {
-
-        $scope.close = function () {
-            // Component lookup should always be available since we are not using `ng-if`
-            $mdSidenav('left').close()
-            .then(function () {
-                //$log.debug("close LEFT is done");
-            });
-        };
-
         $scope.goToState = function(state) {
             $state.go(state);
         }
 
+        $window.onSignIn = onSignIn;
     })
 
     .controller('ToastCtrl', function($scope, $mdToast, $mdDialog) {
