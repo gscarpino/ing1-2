56e0b34ed20c55cf4c74eb6430eb53ea133fa94f
nico <klascolinu@gmail.com>
2016-10-03 00:54:09 -0300

    Agrego getters al bar

--- wifindApp/bar.js
+++ wifindApp/bar.js
@@ -7,6 +7,11 @@ class Bar {
         this.ubicacion = ubicacion;
         this.direccion = direccion;
     }
+
+ 	get nombre(){return this.nombre;}
+ 	get descripcion(){return this.descripcion;}
+ 	get ubicacion(){return this.ubicacion;}
+ 	get direccion(){return this.direccion;}
 }
 
 module.exports = Bar;
