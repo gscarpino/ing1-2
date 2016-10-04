b8c07cacca3314116cbc074ffe5761b9aaae4eb1
nico <klascolinu@gmail.com>
2016-10-04 03:12:24 -0300

    Agrego el sing in de google a el controller de loginCtrl

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,10 +1,51 @@
 angular.module('wifindAppControllers')
-
-    .controller('LoginCtrl', function($scope,$http) {
+  .value('site',{name: 'WifindBar', protocol: 'http', domain: 'localhost', port: '5000'})
+    .controller('LoginCtrl', function($scope,$http,site,$mdToast,$window) {
 
         console.log('Login');
         $scope.showLogin = true;
+        $scope.googleSignedIn = false;
+        $scope.isRegistered = false;
 
+
+        function onSignIn(googleUser) {
+        	console.log('hola');
+            $scope.googleSignedIn = true;
+            $scope.isRegistered = true;
+
+            var profile = googleUser.getBasicProfile();
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail());
+            $scope.userName = profile.getEmail()
+            var user = {
+                nombre: profile.getName(),
+                email: profile.getEmail()
+            };
+
+            $http.post($scope.url + "/api/user/logged",user).then(function(response){
+                    $mdToast.show({
+                        hideDelay   : 1500,
+                        position    : 'top right',
+                        controller  : 'ToastCtrl',
+                        templateUrl : 'toast-template.html'
+                    });
+                },
+                function(error){
+                    console.log("Error", error);
+                });
+        }
+         $scope.signOut = function() {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+                $scope.googleSignedIn = false;
+                $scope.isRegistered = false;
+                console.log('User signed out.');
+            });
+        }
+
+        $window.onSignIn = onSignIn;
+        
         $scope.login = function() {
             $scope.showLogin = true;
             console.log("logueando...")
