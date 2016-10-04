3a944f7e32fce0774726a1284dd55c76152d64de
nico <klascolinu@gmail.com>
2016-10-04 03:11:42 -0300

    Muevo el sign in de google al controller.js

--- app.js
+++ app.js
@@ -43,9 +43,7 @@ angular
     .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, site, $log, $window, $http, $mdToast, $state) {
         $state.go('Inicio');
         $scope.url = site.protocol + "://" + site.domain + ":" + site.port;
-        $scope.googleSignedIn = false;
-
-        $scope.isRegistered = false;
+       
 
         $scope.print = function(){
             console.log(Date.now());
@@ -55,47 +53,11 @@ angular
             return $mdSidenav('right').isOpen();
         };
 
-        $scope.signOut = function() {
-            var auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function () {
-                $scope.googleSignedIn = false;
-                $scope.isRegistered = false;
-                console.log('User signed out.');
-            });
-        }
-
-        function onSignIn(googleUser) {
-            $scope.googleSignedIn = true;
-            $scope.isRegistered = true;
-
-            var profile = googleUser.getBasicProfile();
-            console.log('Name: ' + profile.getName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail());
-            $scope.userName = profile.getEmail()
-            var user = {
-                nombre: profile.getName(),
-                email: profile.getEmail()
-            };
-
-            $http.post($scope.url + "/api/user/logged",user).then(function(response){
-                    $mdToast.show({
-                        hideDelay   : 1500,
-                        position    : 'top right',
-                        controller  : 'ToastCtrl',
-                        templateUrl : 'toast-template.html'
-                    });
-                },
-                function(error){
-                    console.log("Error", error);
-                });
-        }
-
         $scope.goToState = function(state) {
             $state.go(state);
         }
 
-        $window.onSignIn = onSignIn;
+        
     })
 
     .controller('ToastCtrl', function($scope, $mdToast, $mdDialog) {
